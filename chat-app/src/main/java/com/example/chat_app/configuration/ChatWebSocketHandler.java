9ae76bb377728885c253d2b869cd45d0070c4966
sujith <sujithkpg@gmail.com>
2025-03-16 08:02:56 +0530

    chat application initial commit

---
+++ chat-app/src/main/java/com/example/chat_app/configuration/ChatWebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.example.chat_app.configuration;
+
+import org.springframework.web.socket.*;
+
+public class ChatWebSocketHandler implements WebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        session.sendMessage(new TextMessage("Message Received:" + message.getPayload()));
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
